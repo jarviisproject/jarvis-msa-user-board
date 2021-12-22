f090c03ba16bb4c3f7424d66ed3499b40d4b15a3
unknown <lionx3@naver.com>
2021-12-22 18:15:05 +0900

    1222

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = [
     "*"
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,50 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     ),
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-#     ),
-# }
-
-# JWT_AUTH = {
-#     'JWT_ENCODE_HANDLER':
-#         'rest_framework_jwt.utils.jwt_encode_handler',
-#     'JWT_DECODE_HANDLER':
-#         'rest_framework_jwt.utils.jwt_decode_handler',
-#     'JWT_PAYLOAD_HANDLER':
-#         'rest_framework_jwt.utils.jwt_payload_handler',
-#     'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-#         'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-#     'JWT_RESPONSE_PAYLOAD_HANDLER':
-#         'rest_framework_jwt.utils.jwt_response_payload_handler',
-#
-#     'JWT_SECRET_KEY': SECRET_KEY,
-#     'JWT_GET_USER_SECRET_KEY': None,
-#     'JWT_PUBLIC_KEY': None,
-#     'JWT_PRIVATE_KEY': None,
-#     'JWT_ALGORITHM': 'HS256',
-#     'JWT_VERIFY': True,
-#     'JWT_VERIFY_EXPIRATION': True,
-#     'JWT_LEEWAY': 0,
-#     'JWT_EXPIRATION_DELTA': timedelta(days=30),
-#     'JWT_AUDIENCE': None,
-#     'JWT_ISSUER': None,
-#     'JWT_ALLOW_REFRESH': False,
-#     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=30),
-#     'JWT_AUTH_HEADER_PREFIX': 'JWT',
-#     'JWT_AUTH_COOKIE': None,
-# }
 
 REST_USE_JWT = True
-# SOCIAL_OUTH_CONFIG = {
-#     'KAKAO_REST_API_KEY': secrets['KAKAO_REST_API_KEY'],
-#     "KAKAO_REDIRECT_URI": secrets['KAKAO_REDIRECT_URI'],
-#     "KAKAO_SECRET_KEY": secrets['KAKAO_SECRET_KEY']
-# }
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -105,7 +63,7 @@ MIDDLEWARE = [
 ]
 
 # CORS 관련 추가
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000','http://localhost:3000']
+CORS_ORIGIN_WHITELIST = ['https://cors-anywhere.herokuapp.com/http://127.0.0.1:3000','https://cors-anywhere.herokuapp.com/http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'api.urls'
