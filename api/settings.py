7ddaa9d3cc8d39301f563f16c2a1fc190d8e940a
unknown <lionx3@naver.com>
2021-12-22 18:26:55 +0900

    1222

--- api/settings.py
+++ api/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 ]
 
 # CORS 관련 추가
-CORS_ORIGIN_WHITELIST = ['https://cors-anywhere.herokuapp.com/http://127.0.0.1:3000','https://cors-anywhere.herokuapp.com/http://localhost:3000']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000','http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'api.urls'
